f0385438c6309de299ae51d0b7fb3eceaf8e62e4
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-05-15 16:19:56 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var map = [0,0,0,0,0,0,0,0,0];
+
+function generate(){
+    let rand1 = Math.floor(Math.random() * (9 - 1)) + 1;
+    let rand2 = Math.floor(Math.random() * (9 - 1)) + 1;
+
+    let box1 = document.getElementById(rand1);
+    let box2 = document.getElementById(rand2);
+    console.log(rand1,rand2)
+    if(box2 == box1){
+        return generate();
+    }else{
+        map[rand1] = 3;
+        map[rand2] = 3;
+        console.table(map);
+        return box1.innerText = map[rand1], box2.innerText=map[rand2];
+    }
+    
+}
+
+
+
+document.addEventListener("keydown",function(event){
+switch(event.code) {
+    case "KeyS":
+    case "ArrowDown":
+        console.log(event.code);
+        //Math.pow(map[i],2);
+    break;
+    case "KeyW":
+    case "ArrowUp":
+        console.log(event.code);
+
+    break;
+    case "KeyA":
+    case "ArrowLeft":
+        console.log(event.code);
+
+    break;
+    case "KeyD":
+    case "ArrowRight":
+        console.log(event.code);
+
+    break;
+    }
+
+});
+
