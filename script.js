bd70ede42639add656f0bdd63a0db90658b2f724
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-05-26 23:16:09 +0200

    added arrays for directions, small changes for move()

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var map = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arrW = [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15];
+var arrS = [15,11,7,3,14,10,6,2,13,9,5,1,12,8,4,0];
+var arrA = [12,13,14,15,8,9,10,11,4,5,6,7,0,1,2,3]; // L > P
+var arrD = [3,2,1,0,7,6,5,4,11,10,9,8,15,14,13,12]; // P > L
 
 function wynik(){//count score
     var suma = 0;
@@ -33,7 +37,7 @@ function update(){ //generate,update map
             map[rand] = 3;
             console.table("Wynik >= 6: ",wynik());
             scoreCount.innerText = "Score: "+wynik();
-            animate(rand);
+            //animate(rand);
             return getValue(), checkMap();
         }
     }
@@ -47,15 +51,21 @@ function update(){ //generate,update map
             map[rand2] = 3;
             console.log("Wynik < 6: ",wynik());
             scoreCount.innerText = "Score: "+wynik();
-            animate(rand1,rand2)
+            //animate(rand1,rand2)
             return getValue(), checkMap();
         }
 }
 
 document.addEventListener("keydown",function move(event){ //take input and modify map accordingly
-switch(event.code) {
+switch(event.code) {                                      // 1st check if same powers,then check if empty
     case "KeyS":
     case "ArrowDown":
+        arrS.forEach(e => {
+            if(map[e] == map[e+4]){
+                map[e+4] = map[e+4]*3; 
+                map[e]=0;
+            }
+        });
         for(let i = 0; i <= map.length; i++){  
             if(map[i] > 0){
 
@@ -63,21 +73,20 @@ switch(event.code) {
                     map[i+4] = map[i];
                     map[i] = 0;   
                 }
-
-                if(map[i] == map[i+4]){
-                    //console.log("Power")
-                    map[i+4] = map[i+4]*3; 
-                    map[i]=0;
-                }
-
                 if(map[i] != map[i+4]){
-                    //idk
+                    console.log("idk");
                 }
             }
         }
         return update();
     case "KeyW":
     case "ArrowUp":
+        arrW.forEach(e => {
+            if(map[e] == map[e-4]){
+                map[e-4] = map[e-4]*3; 
+                map[e]=0;
+            }
+        });
         for(let i = 15; i >= 0; i--){  
             if(map[i] > 0){
 
@@ -85,21 +94,22 @@ switch(event.code) {
                     map[i-4] = map[i];
                     map[i] = 0;   
                 }
-
-                if(map[i] == map[i-4]){
-                    //console.log("Power")
-                    map[i-4] = map[i-4]*3; 
-                    map[i]=0;
-                }
-
                 if(map[i] != map[i-4]){
-                    //idk
+                    console.log("idk");
                 }
             }
         }
         return update();
     case "KeyA":
     case "ArrowLeft":
+        arrA.forEach(e => {
+            if(e != 0 && e != 4 && e != 8 && e != 12){
+                if(map[e] == map[e-1]){
+                    map[e-1] = map[e-1]*3; 
+                    map[e]=0;
+                }
+            }
+        });
         for(let i = 15; i >= 0; i--){  
             if(i != 0 && i != 4 && i != 8 && i != 12){
                 if(map[i] > 0){
@@ -107,12 +117,6 @@ switch(event.code) {
                         map[i-1] = map[i];
                         map[i] = 0;     
                     }
-    
-                    if(map[i] == map[i-1]){
-                        map[i-1] = map[i-1]*3; 
-                        map[i]=0;
-                    }
-    
                     if(map[i] != map[i-1]){
                         console.log("idk");
                     }
@@ -122,19 +126,21 @@ switch(event.code) {
         return update();
     case "KeyD":
     case "ArrowRight":
+        arrD.forEach(e => {
+            if(e != 3 && e != 7 && e != 11 && e != 15){
+                if(map[e] == map[e+1]){
+                    map[e+1] = map[e+1]*3; 
+                    map[e]=0;
+                }
+            }
+        });
         for(let i = 0; i <= map.length; i++){  
             if(i != 3 && i != 7 && i != 11 && i != 15){
                 if(map[i] > 0){
                     if(map[i+1] == 0){
                         map[i+1] = map[i];
                         map[i] = 0;     
-                    }
-    
-                    if(map[i] == map[i+1]){
-                        map[i+1] = map[i+1]*3; 
-                        map[i]=0;
-                    }
-    
+                    } 
                     if(map[i] != map[i+1]){
                         console.log("idk");
                     }
